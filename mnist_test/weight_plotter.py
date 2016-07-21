b1db3835f6c98ca66157dc2bad38f1686be1b27a
botcs <bcs@bcs.hu>
2016-07-21 10:41:35 +0200

    add visualizing script

--- mnist_test/weight_plotter.py
+++ mnist_test/weight_plotter.py
@@ -1,13 +1,14 @@
-%matplotlib inline
 import matplotlib.pyplot as plt
 import network_module as nm
+
+
 def wplot(activations):
-	for i, w in enumerate(activations):
-	    plt.subplot(1, 10, i + 1)
-	    plt.set_cmap('gray')
-	    plt.axis('off')
-	    plt.imshow(w)
-	plt.gcf().set_size_inches(9, 9)
-	plt.show()
+        for i, w in enumerate(activations):
+            plt.subplot(1, 10, i + 1)
+            plt.set_cmap('gray')
+            plt.axis('off')
+            plt.imshow(w, interpolation='None')
+        plt.gcf().set_size_inches(9, 9)
+        plt.show()
 
 
