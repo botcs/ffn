6f2944980666d7f166b278818161851ab1db41c3
botcs <bcs@bcs.hu>
2016-07-29 14:39:31 +0200

    convnet biased

--- src/conv_module.py
+++ src/conv_module.py
@@ -31,7 +31,7 @@ class conv(lm._layer):
             'For fully connected next layer'
             self.width = np.prod(self.shape)
             
-        self.bias = np.random.randn(self.shape)
+        self.bias = np.random.randn(*self.shape)
 
     def get_local_output(self, input):
         assert type(input) == np.ndarray
@@ -50,7 +50,7 @@ class conv(lm._layer):
             [[[convolve2d(channel, kernel, 'valid')
                for kernel in kernel_set]
               for channel, kernel_set in zip(sample, self.kernels)]
-             for sample in self.input], axis=1) + self.bias
+             for sample in self.input], axis=1)
 
     def backprop_delta(self, target):
         self.delta = self.next.backprop_delta(target).reshape(self.shape)
